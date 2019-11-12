d2b62159aa109e29a54a40d12a1e42e878ab2835
Gabriel Alves <gabrielalves98@hotmail.com>
2019-11-11 21:48:00 -0300

    Acompanhamento Online User

--- smart-management/routes/onlineTracking.js
+++ smart-management/routes/onlineTracking.js
@@ -13,4 +13,10 @@ router.get('/', (req, res) => {
   res.render('manager/onlineTracking', { title: 'Acompanhamento online', layout: 'layoutdashboardmanager' });
 });
 
+router.get('/user/:id', (req, res) => {
+  Station.getById(req.params.id).then((station) => {
+    res.render('manager/onlineTrackingUser', { title: 'Acompanhamento Online', station });
+  });
+});
+
 module.exports = router;
